2770e736f3d94083ed18188db7ef24b8ed1ca108
gkxaaa <gkxaaa@gmail.com>
2019-12-04 14:09:42 +0100

    Update CopyListRandomPointer.py

--- 30.11.2019-leetcode138/CopyListRandomPointer.py
+++ 30.11.2019-leetcode138/CopyListRandomPointer.py
@@ -9,10 +9,15 @@ class Node(object):
 class Solution(object):
     '''
     一道类似于有向图遍历的题。
-    思路1: bfs，每次遍历当前节点的next和random后并标记当前节点，然后对next和random节点重复操作（注意如果next和random节点是一个节点的话操作一次）。用队列存放接下来还需要遍历的节点，若队列空说明所有节点都被访问。
-    思路2：dfs，停止边界是每个节点的next和random若都被访问过，标记当前节点。但有一个问题，若先访问next节点一直到头发现有环，那么递归将不会停止maximum recursion depth exceeded.所以另一个边界是每个节点的next或者random都被访问过。
+    思路1: bfs，每次遍历当前节点的next和random后并标记当前节点，然后对next和random节点重复操作
+         （注意如果next和random节点是一个节点的话操作一次）。用队列存放接下来还需要遍历的节点，若队列空说明所有节点都被访问。
+    思路2：dfs，停止边界是当前节点被访问过或者下一个点为空，每次访问当前节点时，搞定cur.next或cur.random然后标记cur。
     '''
     def copyRandomList_bfs(self, head):
+        """
+        :type head: Node
+        :rtype: Node
+        """
         if not head: return None
         node = Node(head.val, None, None)
         queue = [(head,node)]
@@ -35,17 +40,14 @@ class Solution(object):
     def copyRandomList(self, head):
         if not head: return None
         node = Node(head.val, None, None)
-        d1, d2 = {}, {}
-        d1[node.val]= node
+        d1 = {}
         def dfs(head, node):
-            if d2.get(head.val, 0)>=1:
+            if head.val in d1:
                 return 
             d1[node.val] = node
-            d2[node.val] = d2.get(node.val,0) + 1 # next Node is counted whether it None or not
             if head.next:
                 node.next = d1.get(head.next.val, Node(head.next.val, None, None))
                 dfs(head.next, node.next)
-            d2[node.val] = d2.get(node.val,0) + 1
             if head.random:
                 node.random = d1.get(head.random.val, Node(head.random.val, None, None))
                 dfs(head.random, node.random)
